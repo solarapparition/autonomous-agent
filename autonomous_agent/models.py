186c47450dbad5d2b7dbeab6d5378730a83662e8
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2024-05-07 20:44:44 -0400

    add tools

---
+++ autonomous_agent/models.py
@@ -0,0 +1,98 @@
+"""Model utilities."""
+
+from functools import wraps
+from typing import Callable, Generic, Sequence, TypeVar
+
+from colorama import Fore
+from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import BaseMessage  # , AIMessage, ToolCall
+from langchain_anthropic import ChatAnthropic
+
+# from langchain_core.tools import tool
+
+
+OPUS = "claude-3-opus-20240229"
+CORE_MODEL = ChatAnthropic(temperature=0.8, model=OPUS, verbose=False, max_tokens_to_sample=4096)
+
+
+def format_messages(messages: Sequence[BaseMessage]) -> str:
+    """Format model messages into something printable."""
+    return "\n\n---\n\n".join(
+        [f"[{message.type.upper()}]:\n\n{message.content}" for message in messages]  # type: ignore
+    )
+
+
+T = TypeVar("T")
+QueryFunction = Callable[..., T]
+# QueryFunction = Generic[T]
+
+
+def wrap_printout(
+    query_func: QueryFunction[T], color: str, preamble: str | None, printout: bool
+) -> QueryFunction[T]:
+    """Wrap a query function to print the result."""
+
+    @wraps(query_func)
+    def wrapper(*args: object, **kwargs: object) -> T:
+        if preamble is not None and printout:
+            print(f"\033[1;34m{preamble}\033[0m")
+        result = query_func(*args, **kwargs)
+        if printout:
+            print(f"{color}{result}{Fore.RESET}")
+        return result
+
+    return wrapper
+
+
+async def query_model(
+    messages: Sequence[BaseMessage],
+    model: BaseChatModel = CORE_MODEL,
+    color: str = Fore.RESET,
+    preamble: str | None = None,
+    printout: bool = True,
+) -> str:
+    """Query an LLM chat model. `preamble` is printed before the result."""
+
+    def query(messages: Sequence[BaseMessage]) -> str:
+        return str(model.invoke(messages).content)
+
+    wrapped_query = (
+        wrap_printout(query, color, preamble, printout) if printout else query
+    )
+    return wrapped_query(messages)
+
+    # if preamble is not None and printout:
+    #     print(f"\033[1;34m{preamble}\033[0m")
+    # result = str(model.invoke(messages).content)
+    # if printout:
+    #     print(f"{color}{result}{Fore.RESET}")
+    # return result
+
+
+# def query_model_tool_call(
+#     model: BaseChatModel,
+#     messages: Sequence[BaseMessage],
+#     tools: Sequence[Callable[..., Any]],
+#     color: str = Fore.RESET,
+#     preamble: str | None = None,
+#     printout: bool = True,
+# ) -> list[ToolCall]:
+#     """Query an LLM chat model, returning the result of a tool call."""
+#     model = model.bind_tools([tool(t) for t in tools])
+#     if preamble is not None and printout:
+#         print(f"\033[1;34m{preamble}\033[0m")
+#     result: AIMessage = model.invoke(messages)
+#     call_results = result.tool_calls
+#     if printout:
+#         print(f"{color}{call_results}{Fore.RESET}")
+#     return call_results
+
+
+# def call_tools(
+#     tools: Sequence[Callable[..., Any]], call_results: list[dict[str, Any]]
+# ) -> list[dict[str, Any]]:
+#     """Run the tools on the call args."""
+#     for call_result in call_results:
+#         next_tool = next(t for t in tools if t.__name__ == call_result["name"])
+#         call_result["output"] = next_tool(**call_result["args"])
+#     return call_results
---
+++ autonomous_agent/text.py
@@ -0,0 +1,91 @@
+"""Extract blocks from text."""
+
+from dataclasses import dataclass
+import re
+from textwrap import dedent, indent
+
+
+def dedent_and_strip(text: str) -> str:
+    """Dedent and strip text."""
+    return dedent(text).strip()
+
+
+@dataclass(frozen=True)
+class ExtractionError(Exception):
+    """Raised when an extraction fails."""
+
+    problem: str
+    text: str | None = None
+    start_block_type: str | None = None
+    end_block_type: str | None = None
+
+    @property
+    def message(self) -> str:
+        """Get the error message."""
+        template = """
+        Failed to extract a block:
+        - Start block type: {start_block_type}
+        - End block type: {end_block_type}
+        - Problem: {problem}
+        - Text:
+        {text}
+        """
+        text = indent(str(self.text or "N/A"), "  ")
+        output = template.format(
+            start_block_type=self.start_block_type or "N/A",
+            end_block_type=self.end_block_type or "N/A",
+            problem=self.problem or "N/A",
+            text=text,
+        )
+        return dedent_and_strip(output)
+
+
+def extract_block(text: str, block_type: str) -> str | None:
+    """Extract a code block from the text."""
+    pattern = (
+        r"```{block_type}\n(.*?)```".format(  # pylint:disable=consider-using-f-string
+            block_type=block_type
+        )
+    )
+    match = re.search(pattern, text, re.DOTALL)
+    return match[1].strip() if match else None
+
+
+def extract_blocks(
+    text: str, start_block_type: str, end_block_type: str = "", prefix: str = "```"
+) -> list[str] | None:
+    """Extracts specially formatted blocks of text from the LLM's output. `block_type` corresponds to a label for a markdown code block such as `yaml` or `python`."""
+    pattern = r"{prefix}{start_block_type}\n(.*?){prefix}{end_block_type}".format(  # pylint:disable=consider-using-f-string
+        start_block_type=start_block_type,
+        end_block_type=end_block_type,
+        prefix=prefix,
+    )
+    matches = re.findall(pattern, text, re.DOTALL)
+    return [match.strip() for match in matches] if matches else None
+
+
+def unpack_block(
+    text: str,
+    extracted_result: list[str] | None,
+    start_block_type: str,
+    end_block_type: str,
+) -> str:
+    """Validate and unpack the extracted block."""
+    assert extracted_result and len(extracted_result) == 1, ExtractionError(
+        text=text,
+        start_block_type=start_block_type,
+        end_block_type=end_block_type,
+        problem="Expected exactly one block.",
+    )
+    (block,) = extracted_result
+    return block
+
+
+def extract_and_unpack(
+    text: str, start_block_type: str, end_block_type: str = "", prefix: str = "```"
+) -> str:
+    """Extract and unpack a block."""
+    extracted_result = extract_blocks(
+        text, start_block_type, end_block_type, prefix=prefix
+    )
+    return unpack_block(text, extracted_result, start_block_type, end_block_type)
