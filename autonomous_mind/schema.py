c10845c8943b039e347ab59af0e42c994cbf3a84
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2024-05-18 11:14:02 -0400

    fix printout issue for events

--- autonomous_mind/schema.py
+++ autonomous_mind/schema.py
@@ -2,12 +2,11 @@
 
 from dataclasses import dataclass, field
 from textwrap import indent
-from typing import Any, Literal, Mapping, Self
+from typing import Any, Literal, Mapping, MutableMapping, Self
 from uuid import UUID, uuid4
 
 from autonomous_mind.helpers import (
     Timestamp,
-    as_yaml_str,
     format_timestamp,
     from_yaml_str,
     get_timestamp,
@@ -23,20 +22,25 @@ class FunctionCallEvent:
     "Id of the goal this action is related to."
     summary: str
     content: str
+    type: Literal["function_call"] = "function_call"
     id: UUID = field(default_factory=uuid4)
     timestamp: Timestamp = field(default_factory=get_timestamp)
     success: Literal[-1, 0, 1] = 0
 
     @classmethod
-    def from_mapping(cls, mapping: Mapping[str, Any]) -> Self:
+    def from_mapping(cls, mapping: MutableMapping[str, Any]) -> Self:
         """Create event from a mapping."""
-        return cls(
-            id=UUID(mapping["id"]),
-            goal_id=UUID(mapping["goal_id"]) if mapping["goal_id"] else None,
-            timestamp=format_timestamp(mapping["timestamp"]),
-            summary=mapping["summary"],
-            content=mapping["content"],
-        )
+        mapping["id"] = UUID(mapping["id"])
+        mapping["goal_id"] = UUID(mapping["goal_id"]) if mapping["goal_id"] else None
+        mapping["timestamp"] = format_timestamp(mapping["timestamp"])
+        return cls(**mapping)
+        # return cls(
+        #     id=UUID(mapping["id"]),
+        #     goal_id=UUID(mapping["goal_id"]) if mapping["goal_id"] else None,
+        #     timestamp=format_timestamp(mapping["timestamp"]),
+        #     summary=mapping["summary"],
+        #     content=mapping["content"],
+        # )
 
     def __repr__(self) -> str:
         """Get the string representation of the event."""
@@ -94,20 +98,26 @@ class CallResultEvent:
     function_call_id: UUID
     "Id of the function call this result is for."
     content: str
+    type: Literal["call_result"] = "call_result"
     id: UUID = field(default_factory=uuid4)
     timestamp: Timestamp = field(default_factory=get_timestamp)
     summary: str = ""
 
     @classmethod
-    def from_mapping(cls, mapping: Mapping[str, Any]) -> Self:
+    def from_mapping(cls, mapping: MutableMapping[str, Any]) -> Self:
         """Create an event from a mapping."""
-        return cls(
-            id=UUID(mapping["id"]),
-            goal_id=UUID(mapping["goal_id"]) if mapping["goal_id"] else None,
-            timestamp=format_timestamp(mapping["timestamp"]),
-            function_call_id=UUID(mapping["function_call_id"]),
-            content=mapping["content"],
-        )
+        # return cls(
+        #     id=UUID(mapping["id"]),
+        #     goal_id=UUID(mapping["goal_id"]) if mapping["goal_id"] else None,
+        #     timestamp=format_timestamp(mapping["timestamp"]),
+        #     function_call_id=UUID(mapping["function_call_id"]),
+        #     content=mapping["content"],
+        # )
+        mapping["id"] = UUID(mapping["id"])
+        mapping["goal_id"] = UUID(mapping["goal_id"]) if mapping["goal_id"] else None
+        mapping["timestamp"] = format_timestamp(mapping["timestamp"])
+        mapping["function_call_id"] = UUID(mapping["function_call_id"])
+        return cls(**mapping)
 
     def __repr__(self) -> str:
         """Get the string representation of the event."""
